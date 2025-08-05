b283ca3546fa6c730273c15cebfa96a66225107d
shortydoggg <lnvs5433@aol.com>
2025-08-05 18:20:46 -0400

    update

--- make.sh
+++ make.sh
@@ -1,5 +1,11 @@
-rm -rf cs 
+rm -rf cs/
+rm -rf ui/
+rm -f *.class
+rm -f threephase.jar
+rm -f *.data
+mkdir cs/
+mkdir ui/
+# exit  
 javac -d . -cp twophase.jar src/*.java
-cp src/*.java cs/threephase/
-jar -cfm threephase.jar META-INF/MANIFEST.MF cs
+jar -cfm threephase.jar META-INF/MANIFEST.MF cs/
 
